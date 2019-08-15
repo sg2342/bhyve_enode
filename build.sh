330e12da0ad2c175965a56d42905a1cc0bde9faa
Stefan Grundmann <sg2342@googlemail.com>
2019-08-15 19:05:21 +0000

    build.sh...

--- build.sh
+++ build.sh
@@ -17,14 +17,28 @@ src_conf="${basedir}/src.conf"
 obj_dir_pfx=/usr/obj/bhyve_enode
 export MAKEOBJDIRPREFIX="$obj_dir_pfx"
 export KERNCONFDIR="${basedir}/kernel"
+export SRCCONF="$src_conf"
 
 ## build kernel
 kldload filemon || true
-env SRCCONF="$src_conf" make -s -j8 -C /usr/src  SRC_ENV_CONF="$src_env_conf" \
-    buildkernel
+make -s -j8 -C /usr/src  SRC_ENV_CONF="$src_env_conf" buildkernel
 
 
-## build /bin/sh
-env SRCCONF="$src_conf" make -s -j8 -C /usr/src/bin/sh  SRC_ENV_CONF="$src_env_conf"
-
-
+## build /bin/sh and the libraries needed by it
+## build additional libraries needed by erts runtime
+tgts="bin/sh
+      lib/msun
+      lib/libc
+      lib/libedit
+      lib/ncurses/ncurses
+      lib/ncurses/ncursesw
+      libexec/rtld-elf
+      lib/libutil
+      lib/libdl
+      lib/libelf
+      lib/libz
+      lib/libthr
+      lib/librt"
+for tgt in $tgts
+do make -s -j8 -C /usr/src/"$tgt" SRC_ENV_CONF="$src_env_conf"
+done
